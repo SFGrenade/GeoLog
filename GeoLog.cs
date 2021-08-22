0e796a7c1150b4f41830ee28d841015f491360fa
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2021-08-22 16:00:43 +0200

    ReSharper naming fixes Up version number

--- GeoLog.cs
+++ GeoLog.cs
@@ -2,11 +2,9 @@
 using System.Reflection;
 using Modding;
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine.SceneManagement;
-using Object = UnityEngine.Object;
 using SFCore.Utils;
 
 namespace GeoLog
@@ -17,9 +15,9 @@ namespace GeoLog
 
         public override string GetVersion() => Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-        private List<KeyValuePair<string, string>> doneGos = new List<KeyValuePair<string, string>>();
+        private List<KeyValuePair<string, string>> _doneGos = new List<KeyValuePair<string, string>>();
 
-        private Dictionary<string, string> pdToNameMap = new Dictionary<string, string>();
+        private Dictionary<string, string> _pdToNameMap = new Dictionary<string, string>();
 
         public override List<ValueTuple<string, string>> GetPreloadNames()
         {
@@ -51,7 +49,7 @@ namespace GeoLog
             {
                 if (jes.playerDataName.Equals("Crawler") && jes.nameConvo.Equals("NAME_HOLLOW_SHADE")) continue;
                 Log($"\"{jes.playerDataName}\": \"{jes.nameConvo}\"");
-                pdToNameMap.Add(jes.playerDataName, Language.Language.Get(jes.nameConvo, "Journal"));
+                _pdToNameMap.Add(jes.playerDataName, Language.Language.Get(jes.nameConvo, "Journal"));
             }
 
             Log($"\"Scene Path\", \"GameObject Name\", Small Geo, Medium Geo, Large Geo, Total Geo, Total Geo (Greed)");
@@ -96,14 +94,14 @@ namespace GeoLog
         {
             try
             {
-                if (doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
+                if (_doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
                 int sg = self.GetAttr<HealthManager, int>("smallGeoDrops");
                 int mg = self.GetAttr<HealthManager, int>("mediumGeoDrops");
                 int lg = self.GetAttr<HealthManager, int>("largeGeoDrops");
                 EnemyDeathEffects ede = self.GetAttr<HealthManager, EnemyDeathEffects>("enemyDeathEffects");
                 string name = ede.GetAttr<EnemyDeathEffects, string>("playerDataName");
-                Log($"{self.gameObject.scene.path}, {pdToNameMap[name]}, {sg}, {mg}, {lg}, {sg + (mg * 5) + (lg * 25)}, {Mathf.CeilToInt(sg * 1.2f) + (Mathf.CeilToInt(mg * 1.2f) * 5) + (Mathf.CeilToInt(lg * 1.2f) * 25)}");
-                doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
+                Log($"{self.gameObject.scene.path}, {_pdToNameMap[name]}, {sg}, {mg}, {lg}, {sg + (mg * 5) + (lg * 25)}, {Mathf.CeilToInt(sg * 1.2f) + (Mathf.CeilToInt(mg * 1.2f) * 5) + (Mathf.CeilToInt(lg * 1.2f) * 25)}");
+                _doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
             }
             catch (Exception )
             {
@@ -116,17 +114,17 @@ namespace GeoLog
             {
                 if (self.FsmName.Equals("Geo Rock"))
                 {
-                    if (doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
+                    if (_doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
                     var tmpV = self.FsmVariables;
                     int fp = tmpV.GetFsmInt("Final Payout").Value;
                     int gph = tmpV.GetFsmInt("Geo Per Hit").Value;
                     int h = tmpV.GetFsmInt("Hits").Value;
                     Log($"{self.gameObject.scene.path}, Geo Rock, -, -, -, {(gph * h) + fp}, {(gph * h) + fp}");
-                    doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
+                    _doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
                 }
                 else if (self.FsmName.Equals("Shiny Control"))
                 {
-                    if (doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
+                    if (_doneGos.Exists(x => x.Key == self.gameObject.scene.path && x.Value == self.gameObject.GetGoPath())) return;
                     var tmpV = self.FsmVariables;
                     int trinketNum = tmpV.GetFsmInt("Trinket Num").Value;
                     if (trinketNum > 0 && trinketNum < 5)
@@ -153,7 +151,7 @@ namespace GeoLog
                                 break;
                         }
                         Log($"{self.gameObject.scene.path}, {name}, -, -, -, {ret}, {ret}");
-                        doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
+                        _doneGos.Add(new KeyValuePair<string, string>(self.gameObject.scene.path, self.gameObject.GetGoPath()));
                     }
                 }
             }
